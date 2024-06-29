ed3f3756ddb37f1ae9a574ed2a76225586c6b152
11hertz <reachzin@naver.com>
2024-06-29 15:22:16 +0900

    modify readline, generator

--- js/generator.js
+++ js/generator.js
@@ -1,3 +1,7 @@
+const readline = require('readline');
+const { stdin: input, stdout: output } = require('process');
+const rl = readline.createInterface({ input, output });
+
 // 두 개의 수를 입력 받아 더하기를 수행하는 제너레이터를 작성하시오.
 function* add() {
   const num1 = yield '첫 번째 수?';
@@ -7,10 +11,21 @@ function* add() {
 
 const itAdd = add();
 console.log(itAdd.next().value);
-console.log(itAdd.next(1).value);
-console.log(itAdd.next(2).value);
+// console.log(itAdd.next().value);
+// console.log(itAdd.next(1).value);
+// console.log(itAdd.next(2).value);
 
 // (실행 결과: 1과 2를 넣었을 때)
 // 첫 번째 수?  → 1   next(1)
 // 두 번째 수?  → 2
 // 3
+
+rl.on('line', (answer) => {
+  const { value, done } = itAdd.next(+answer);
+  console.log(value);
+  if (done) {
+    rl.close();
+  }
+}).on('close', () => {
+  process.exit();
+});
--- js/readline.js
+++ js/readline.js
@@ -4,7 +4,6 @@ const { stdin: input, stdout: output } = require('process');
 const rl = readline.createInterface({ input, output });
 
 rl.question('What do you think of Node.js? ', (answer) => {
-  // TODO: Log the answer in a database
   console.log(`Thank you for your valuable feedback: ${answer}`);
 
   rl.close();
@@ -15,4 +14,9 @@ rl.on('close', function () {
 });
 
 // cf. line listener
-rl.on('line', function (line) {});
+rl.on('line', (answer) => {
+  console.log('line.answer>>', answer);
+  if (answer === 'bye') rl.close();
+}).on('close', () => {
+  process.exit();
+});
